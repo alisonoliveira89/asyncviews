77ee59465d45a768f7740ee7a10516d8ed71f2f6
Alison Oliveira <alison.oliveira89@gmail.com>
2025-03-03 13:26:42 -0300

    ajuste home_view

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -28,4 +28,4 @@ def sync_view(request):
     return HttpResponse("Blocking HTTP request")
 
 def home_view(request):
-    return HttpResponse("<br> Aula 5: Views Assincronas com Django Async Views OK <br> 127.0.0.1:8000/ home_view pagina inicial OK<br>127.0.0.1:8000/api/ Non-blocking HTTP request OK <br>127.0.0.1:8000/sync/ Blocking HTTP request OK ")
+    return HttpResponse("<br> Aula 11: Views Assincronas com Django Async Views OK <br> 127.0.0.1:8000/ home_view pagina inicial OK<br>127.0.0.1:8000/api/ Non-blocking HTTP request OK <br>127.0.0.1:8000/sync/ Blocking HTTP request OK ")
